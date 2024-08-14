3f7a220e26acc5e0a6ac5094b6c8f4df3caa6005
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-14 06:00:36 +0200

    Adding Profile Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Profile from "./components/Profile"
+
 function App() {
   return (
     <div>
       <h1>Hello from React and Vite App !</h1>
-      <h2>How are You ?</h2>
+      <h2>How are You ?</h2> <br />
+      <Profile />
     </div>
   )
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>Fullname : John DOE</h1>
+      <p>Emai : johndoe@gmail.com</p>
+      <article>Phone : 0152146912</article>
+    </div>
+  )
+}
+
+export default Profile

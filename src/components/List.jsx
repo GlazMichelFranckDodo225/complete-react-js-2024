90de607160d1c611847744c323bf3e1e8bfd0e31
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-18 10:57:27 +0200

    Dynamic Expressions and Map() Function - V2

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,50 @@
 const List = () => {
-    const technologies = ["Java", "Spring", "React", "Bootstrap", "JavaScript"];
+    const people = [{
+        id: 0,
+        name: 'Creola Katherine Johnson',
+        profession: 'mathématicienne',
+        accomplishment: 'ses calculs pour vol spatiaux',
+        imageId: 'MK3eW3A'
+    }, {
+        id: 1,
+        name: 'Mario José Molina-Pasquel Henríquez',
+        profession: 'chimiste',
+        accomplishment: 'sa découverte du trou dans la couche d’ozone au-dessus de l’Arctique',
+        imageId: 'mynHUSa'
+    }, {
+        id: 2,
+        name: 'Mohammad Abdus Salam',
+        profession: 'physicien',
+        accomplishment: 'sa théorie de l’électromagnétisme',
+        imageId: 'bE7W1ji'
+    }, {
+        id: 3,
+        name: 'Percy Lavon Julian',
+        profession: 'chimiste',
+        accomplishment: 'ses travaux pionniers sur la cortisone, les stéroïdes et les pilules contraceptives',
+        imageId: 'IOjWm71'
+    }, {
+        id: 4,
+        name: 'Subrahmanyan Chandrasekhar',
+        profession: 'astrophysicien',
+        accomplishment: 'son calcul de la masse des naines blanches',
+        imageId: 'lrWQx8l'
+    }];
+
+    // const technologies = ["Java", "Spring", "React", "Bootstrap", "JavaScript", "Python"];
 
     return (
         <div>
-            <h5>Technologies :</h5>
-            <ul>
-                {technologies.map(item => (
-                    <li>{item}</li>
-                ))}
-            </ul>
+            <h5>People :</h5>
+            {people.map(item => (
+                <div key={item.id}>
+                    <div>
+                        <p>Name : {item.name}</p>
+                        <p>Profession : {item.profession}</p>
+                        <hr />
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
